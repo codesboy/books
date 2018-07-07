12a6ba3d919d7789e0648c6761ebd9ff19c8871d
codesboy <565195693@qq.com>
2018-07-07 18:50:27 +0800

    update

--- public/wx/pages/analyse/analyse.js
+++ public/wx/pages/analyse/analyse.js
@@ -3,17 +3,19 @@
 const app = getApp();
 Page({
 
-  /**
-   * 初始数据
-   */
-  data: {
-    data:"",
-    // debtsData:""
-  },
-    onShow:function(){
+    /**
+     * 初始数据
+     */
+    data: {
+        data: "",
+        last_debts:0
+        // debtsData:""
+    },
+    onShow: function() {
         this.loadData();
     },
 
+    // 请求数据
     loadData() {
         wx.request({
             url: app.globalData.baseUrl + 'getalldebts',
@@ -21,14 +23,16 @@ Page({
             success: (res) => {
                 this.setData({
                     data: res.data,
-                    // debtsData:res.data.debts
-                })
+                    last_debts: this.dataOperation(res.data)
+                });
+                // console.log(this.dataOperation(res.data))
+                // this.dataOp600eration(res.data)
 
             },
-            fail: function (e) {
+            fail: function(e) {
                 console.log(e.errMsg)
             },
-            complete: function () {
+            complete: function() {
                 wx.stopPullDownRefresh();
                 if (wx.hideLoading) {
                     wx.hideLoading();
@@ -36,4 +40,31 @@ Page({
             }
         });
     },
+
+    // 数据运算处理
+    dataOperation: (data) => {
+        let arr=[];
+        for (let i in data) {
+        let c = 0;
+            for (let j in data[i].debts) {
+                let a = data[i].debts[j].debts_money;
+                let b = data[i].debts[j].payback_money;
+                let r = a - b;
+                c += r;
+                // console.log(c)
+            }
+            arr.push(c);
+        }
+        return arr;
+    },
+
+    readDetail:(e)=>{
+        console.log()
+        let cid = e.currentTarget.dataset.id;
+        wx.navigateTo({
+            url: 'detail/detail?cid='+cid,
+        })
+    }
+
+
 })
